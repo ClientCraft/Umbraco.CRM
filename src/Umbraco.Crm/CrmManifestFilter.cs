d18a18e7729923d7316df48b7a10ec930d9c0875
Arthur Nassar <arthurnassar@gmail.com>
2025-04-07 19:13:26 +0100

    adjust stuff

--- src/Umbraco.Crm/CrmManifestFilter.cs
+++ src/Umbraco.Crm/CrmManifestFilter.cs
@@ -1,28 +1,50 @@
 using Umbraco.Cms.Core.Manifest;
 
 namespace Umbraco.Community.Umbraco.Crm;
+
 internal class CrmManifestFilter : IManifestFilter
 {
     public void Filter(List<PackageManifest> manifests)
     {
         var assembly = typeof(CrmManifestFilter).Assembly;
 
-        manifests.Add(new PackageManifest
+        var packageManifest = new PackageManifest
         {
             PackageName = "Umbraco.Crm",
             Version = assembly.GetName()?.Version?.ToString(3) ?? "0.1.0",
             AllowPackageTelemetry = true,
-            Scripts = new string[] {
-                // List any Script files
+            Scripts = new string[]
+            {
+                    // List any Script files
                 // Urls should start '/App_Plugins/Umbraco.Crm/' not '/wwwroot/Umbraco.Crm/', e.g.
-                // "/App_Plugins/Umbraco.Crm/Scripts/scripts.js"
+                "/App_Plugins/Umbraco.Crm/app/app.component.js",
+                "/App_Plugins/Umbraco.Crm/app/components/lead/list/lead-list.component.js",
+                "/App_Plugins/Umbraco.Crm/app/components/deal/list/deal-list.component.js",
+                "/App_Plugins/Umbraco.Crm/app/components/contact/list/contact-list.component.js",
+                "/App_Plugins/Umbraco.Crm/app/components/account/list/account-list.component.js"
             },
             Stylesheets = new string[]
             {
                 // List any Stylesheet files
                 // Urls should start '/App_Plugins/Umbraco.Crm/' not '/wwwroot/Umbraco.Crm/', e.g.
                 // "/App_Plugins/Umbraco.Crm/Styles/styles.css"
-            }
-        });
+            },
+            Sections =
+            [
+                new ManifestSection
+                {
+                    Alias = "Umbraco.Crm", Name = "CRM"
+                }
+            ],
+            Dashboards =
+            [
+                new ManifestDashboard
+                {
+                    Alias = "Umbraco.Crm.Main.Dashboard", View = "/App_Plugins/Umbraco.Crm/index.html", Sections = ["Umbraco.Crm"], Weight = 10
+                }
+            ]
+        };
+
+        manifests.Add(packageManifest);
     }
 }
---
+++ src/Umbraco.Crm/wwwroot/Trees/LeadsTreeController.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Umbraco.Cms.Core;
+using Umbraco.Cms.Core.Actions;
+using Umbraco.Cms.Core.Events;
+using Umbraco.Cms.Core.Models.Trees;
+using Umbraco.Cms.Core.Services;
+using Umbraco.Cms.Core.Trees;
+using Umbraco.Cms.Web.BackOffice.Trees;
+using Umbraco.Cms.Web.Common.Attributes;
+using Umbraco.Extensions;
+
+namespace Umbraco.Community.Umbraco.Crm.wwwroot.Trees;
+
+[Tree("Umbraco.Crm", "leads", TreeTitle = "Leads", TreeGroup = "crmItemsGroup", SortOrder = 1)]
+[PluginController("UmbracoCrm")]
+public class LeadsTreeController : TreeController
+{
+
+    private readonly IMenuItemCollectionFactory _menuItemCollectionFactory;
+
+    public LeadsTreeController(ILocalizedTextService localizedTextService,
+        UmbracoApiControllerTypeCollection umbracoApiControllerTypeCollection,
+        IMenuItemCollectionFactory menuItemCollectionFactory,
+        IEventAggregator eventAggregator)
+        : base(localizedTextService, umbracoApiControllerTypeCollection, eventAggregator) => _menuItemCollectionFactory = menuItemCollectionFactory ?? throw new ArgumentNullException(nameof(menuItemCollectionFactory));
+
+    protected override ActionResult<TreeNodeCollection> GetTreeNodes(string id, FormCollection queryStrings)
+    {
+       return new TreeNodeCollection();
+    }
+
+    protected override ActionResult<MenuItemCollection> GetMenuForNode(string id, FormCollection queryStrings)
+    {
+        // create a Menu Item Collection to return so people can interact with the nodes in your tree
+        var menu = _menuItemCollectionFactory.Create();
+        return menu;
+
+    }
+
+    protected override ActionResult<TreeNode> CreateRootNode(FormCollection queryStrings)
+    {
+        var rootResult = base.CreateRootNode(queryStrings);
+        if (!(rootResult.Result is null))
+        {
+            return rootResult;
+        }
+
+        var root = rootResult.Value;
+
+        //optionally setting a routepath would allow you to load in a custom UI instead of the usual behaviour for a tree
+        root.RoutePath = string.Format("{0}/{1}/{2}", "Umbraco.Crm", "leads", "overview");
+        // set the icon
+        root.Icon = "icon-hearts";
+        // set to false for a custom tree with a single node.
+        root.HasChildren = false;
+        //url for menu
+        root.MenuUrl = null;
+
+        return root;
+    }
+}
