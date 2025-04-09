a538a63fc45e146c62b217824951fb498c4a2741
Arthur Nassar <arthurnassar@gmail.com>
2025-04-09 22:21:43 +0100

    push stuff

--- src/Umbraco.Crm/wwwroot/UmbracoCrm/backoffice/leads/overview.controller.js
+++ src/Umbraco.Crm/wwwroot/UmbracoCrm/backoffice/leads/overview.controller.js
@@ -1,8 +1,11 @@
-angular
-  .module("umbraco")
-  .controller("Umbraco.Crm.Leads.OverviewController", function ($scope) {
-    $scope.title = "Leads";
-    $scope.buttons = [{
+angular.module("umbraco").controller("Umbraco.Crm.Leads.OverviewController", function ($scope, $http) {
+  var vm = this;
+
+  // Initial Setup
+  vm.title = "Leads";
+  vm.allowSelectAll = true;
+  vm.buttons = [
+    {
       label: "Create Lead",
       icon: "icon-plus",
       action: function () {
@@ -17,7 +20,8 @@ angular
         alert("Import Leads clicked");
       },
       isDisabled: false
-    }, {
+    },
+    {
       label: "Export to file",
       icon: "icon-cloud-download",
       action: function () {
@@ -25,5 +29,46 @@ angular
       },
       isDisabled: false
     }
-  ]
-  });
+  ];
+
+  // Fetch Leads Data
+  $http
+    .get("http://foo.localhost:8000/lead?include=status,notes,owner,owner.photo,photo,deputies,tags,tasks&includeConvertedLeads=false&includeCompletedTasks=false&page=1")
+    .then(function (response) {
+      vm.items = response.data.data.items;
+      vm.options = {
+        includeProperties: response.data.data.headers.map(x => ({
+          alias: x.field,
+          header: x.name
+        }))
+      };
+    })
+    .catch(function (error) {
+      console.error("Error fetching leads:", error);
+    });
+
+  // Table Event Handlers
+  vm.selectItem = function (item, $index, $event) {
+    alert("Selected: " + item.name);
+  };
+
+  vm.clickItem = function (item) {
+    alert("Clicked: " + item.name);
+  };
+
+  vm.selectAll = function ($event) {
+    alert("Select All toggled");
+  };
+
+  vm.isSelectedAll = function () {
+    return false; // Logic for checking if all items are selected
+  };
+
+  vm.isSortDirection = function (col, direction) {
+    return "asc"; // Return sort direction (e.g., 'asc' or 'desc')
+  };
+
+  vm.sort = function (field, allow, isSystem) {
+    alert("Sorting by " + field);
+  };
+});
